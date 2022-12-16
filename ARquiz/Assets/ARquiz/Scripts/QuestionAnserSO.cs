395b753512a9df84822b58effde7fbf6e0f6708b
DESKTOP-5RE5JM0\Dawud <dawud.albouz96@gmail.com>
2022-12-16 01:14:53 +0100

    Q & A scriptable Object created

---
+++ ARquiz/Assets/ARquiz/Scripts/QuestionAnserSO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Q&A")]
+public class QuestionAnserSO : ScriptableObject
+{
+    [TextArea]
+    public string QuestionCro;
+
+    [TextArea]
+    public string QuestionEng;
+
+    public List<string> Ansers;
+
+    public int CorrectAnser;
+}
