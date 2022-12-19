b3dcc07e55c33c622e4c40ac8cf5ae082d1436bf
DESKTOP-5RE5JM0\Dawud <dawud.albouz96@gmail.com>
2022-12-19 01:47:34 +0100

    create const string variables

---
+++ ARquiz/Assets/ARquiz/Constantes/StringConst.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StringConst 
+{
+    /*
+     
+     */
+
+    public static string GameObjectTag = "ARObject";
+    public static string MainMenuTxt = "Glavni Meni";
+    public static string InfoScreenTxt = "Info";
+    public static string ARScreenTxt = "AR";
+    public static string QuizScreenTxt = "Kviz";
+}
